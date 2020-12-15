d4c954ebc731b29e6c93bbe3b08a5549cdbacd8a
Satadru Roy <satadru.roy98@gmail.com>
2020-12-15 18:51:21 +0530

    Not much changed... Working on side drawer

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,7 +1,9 @@
 const ready = () => {
+  // Positioning hero elements realative to the phone-bezel
+  const mainBody = document.getElementById('body');
   const phoneBezelDiv = document.getElementById('phone-bezel-div');
 
-  const scrolledPosition = document.documentElement.scrollTop || document.body.scrollTop;
+  const scrolledPosition = mainBody.scrollTop;
   const psuedoElementHeight = +window
     .getComputedStyle(phoneBezelDiv, '::before')
     .getPropertyValue('height')
@@ -37,35 +39,94 @@ const ready = () => {
     editedPhoneBezelDivSizes.x + editedPhoneBezelDivSizes.width * 1.25 + 'px',
   );
 
+  // Navbar Changing Color while scrolling
   const navbar = document.getElementById('navbar');
   const onScroll = () => {
-    const scrolledPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    const scrolledPosition = mainBody.scrollTop;
+    document.body.style.setProperty('--view-top', scrolledPosition);
     navbar.style.backgroundColor = 'rgba(254, 205, 26,' + scrolledPosition * 0.007 + ')';
-  };
-
-  const backdrop = document.getElementById('backdrop');
-  const sideDrawer = document.getElementById('mobile-nav');
-  const menuToggle = document.getElementById('navi-toggle');
-
-  const backdropClickHandler = () => {
-    backdrop.style.display = 'none';
-    sideDrawer.classList.remove('open');
-    menuToggle.checked = false;
-  };
-
-  const menuToggleClickHandler = () => {
-    if (menuToggle.checked) {
-      backdrop.style.display = 'block';
-      sideDrawer.classList.add('open');
-    } else {
-      backdropClickHandler();
+    if (scrolledPosition * 0.0007 <= 0.1) {
+      navbar.style.boxShadow = '0 0 1rem rgba(0, 0, 0,' + scrolledPosition * 0.0007 + ')';
     }
   };
+  mainBody.onscroll = onScroll;
 
-  backdrop.addEventListener('click', backdropClickHandler);
-  menuToggle.addEventListener('change', menuToggleClickHandler);
+  // // Adding the Side-Drawer & backdrop on click
+  // const backdrop = document.getElementById('backdrop');
+  // const sideDrawer = document.getElementById('mobile-nav');
+  // const menuToggle = document.getElementById('navi-toggle');
 
-  window.onscroll = onScroll;
+  // const backdropClickHandler = () => {
+  //   backdrop.style.display = 'none';
+  //   sideDrawer.classList.remove('open');
+  //   menuToggle.checked = false;
+  // };
+
+  // const menuToggleClickHandler = () => {
+  //   if (menuToggle.checked) {
+  //     backdrop.style.display = 'block';
+
+  //     sideDrawer.classList.add('open');
+  //   } else {
+  //     backdropClickHandler();
+  //   }
+  // };
+
+  // backdrop.addEventListener('click', backdropClickHandler);
+  // menuToggle.addEventListener('change', menuToggleClickHandler);
+
+  // // Adding Swipe gestures to pull the Side-drawer out (for mobile-users)
+  // let x0 = null;
+
+  // const moveAndOtherListenersRemoved = ev => {
+  //   let dx = ev.changedTouches[0].clientX - x0;
+  //   let s = Math.sign(dx);
+
+  //   if (s > 0) {
+  //     menuToggle.checked = true;
+  //     menuToggleClickHandler();
+  //   }
+  //   if (s < 0) {
+  //     menuToggle.checked = false;
+  //     menuToggleClickHandler();
+  //   }
+
+  //   x0 = null;
+
+  //   sideDrawer.style.transition = 'transform 0.5s ease-out';
+  //   if (
+  //     sideDrawer.getBoundingClientRect().left / sideDrawer.getBoundingClientRect().width >=
+  //     -0.75
+  //   ) {
+  //     sideDrawer.classList.add('open');
+  //     sideDrawer.style.setProperty('--perct', '0px');
+  //   }
+
+  //   window.ontouchmove = null;
+  //   window.ontouchend = null;
+  // };
+
+  // const drag = ev => {
+  //   if (sideDrawer.getBoundingClientRect().left >= 0) {
+  //     return;
+  //   }
+  //   sideDrawer.style.setProperty('--perct', Math.round(ev.changedTouches[0].clientX - x0) + 'px');
+  // };
+
+  // const lockAndOtherListenersAdded = ev => {
+  //   if (!ev.changedTouches) {
+  //     return;
+  //   }
+  //   x0 = ev.changedTouches[0].clientX;
+  //   sideDrawer.style.transition = 'none';
+  //   console.log(ev);
+  //   console.log(ev.changedTouches[0]);
+  //   console.log(ev.changedTouches[0].clientX);
+  //   window.ontouchmove = drag;
+  //   window.ontouchend = moveAndOtherListenersRemoved;
+  // };
+
+  // window.addEventListener('touchstart', lockAndOtherListenersAdded);
 };
 
 window.onload = ready;
