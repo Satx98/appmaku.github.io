c42c4bfaf8353dd5855a501c4ec5145925885bc0
Satadru Roy <satadru.roy98@gmail.com>
2020-12-19 04:05:42 +0530

    used requestAnimation instead of CSS animation

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,29 +1,47 @@
-const changeInMainBodyWidthEvent = new Event('changeInMainBodyWidth');
-
 const sideDrawer = document.getElementById('mobile-nav');
 const mainBody = document.getElementById('body');
 
 const backdrop = document.getElementById('backdrop');
 const menuToggle = document.getElementById('navi-toggle');
 const navbar = document.getElementById('navbar');
+const navIcon = document.getElementById('navigation-icon');
 const phoneBezelDiv = document.getElementById('phone-bezel-div');
 const accompanyOneEl = document.getElementById('accompany-1');
 const bgImg1 = document.getElementById('bg-img-1');
 
 let bodySizes = null;
-let interval1 = null;
-let timeout1 = null;
+let timeoutCounter = null;
+let fps = null;
 
 let mainBodyShrinkSize = 0; // Add media queries later here
 let sideDrawerWidth = 0;
 let factor = 0;
 
-const onChangeInMainBodyWidth = ev => {
-  factor = ev
-    ? ev.target.offsetLeft / sideDrawerWidth
-    : mainBody.getBoundingClientRect().x / sideDrawerWidth;
-  document.body.style.setProperty('--f', factor.toFixed(2));
-  backdrop.style.opacity = factor.toFixed(2);
+const animate = item => {
+  const duration = item.time * 1000;
+  const end = Date.now() + duration;
+
+  const step = () => {
+    const current = Date.now();
+    const remaining = end - current;
+
+    if (remaining < 30) {
+      if (item.backward) {
+        item.run(1);
+      } else {
+        item.run(0);
+      }
+      return item.onComplete ? item.onComplete() : null;
+    } else {
+      const rate = item.backward
+        ? 1 - Math.sin(Math.pow(remaining / duration, 3))
+        : Math.sin(Math.pow(remaining / duration, 3));
+      // console.log(rate);
+      item.run(rate);
+    }
+    requestAnimationFrame(step);
+  };
+  step();
 };
 
 // Navbar Changing Color while scrolling
@@ -35,69 +53,39 @@ const onScroll = () => {
   }
 };
 
-// Making and triggering a custom event changeInMainBodyWidth
-let fps = null;
-let tempX = null;
-let counterToStopFps = 0;
-
-const stopfps = () => {
-  clearInterval(fps);
-  fps = null;
-  tempX = null;
-  counterToStopFps = 0;
-
-  menuToggle.classList.remove('.button-icon-animation--1');
-  mainBody.classList.remove('smooth-animation--1');
-  navbar.classList.remove('smooth-animation--1');
-  navbar.previousElementSibling.classList.remove('smooth-animation--1');
-
-  menuToggle.classList.remove('.button-icon-animation--2');
-  mainBody.classList.remove('smooth-animation--2');
-  navbar.classList.remove('smooth-animation--2');
-  navbar.previousElementSibling.classList.remove('smooth-animation--2');
-
-  menuToggle.classList.remove('.button-icon-animation--3');
-  mainBody.classList.remove('smooth-animation--3');
-  navbar.classList.remove('smooth-animation--3');
-  navbar.previousElementSibling.classList.remove('smooth-animation--3');
-
-  if (!menuToggle.checked) {
-    backdrop.classList.remove('backdrop-display');
-  }
-};
-
-const triggeringChangeInMainBodyWidth = () => {
-  if (tempX === mainBody.offsetLeft) {
-    counterToStopFps++;
-    if (counterToStopFps >= 10) {
-      stopfps();
-    }
-    return;
-  }
-  mainBody.dispatchEvent(changeInMainBodyWidthEvent);
-  tempX = mainBody.offsetLeft;
-};
-
-const startfps = () => {
-  fps = setInterval(triggeringChangeInMainBodyWidth, 16);
-};
-
 // Menu Button Click Handler
 const menuToggleClickHandler = () => {
-  menuToggle.classList.add('.button-icon-animation--1');
-  mainBody.classList.add('smooth-animation--1');
-  navbar.classList.add('smooth-animation--1');
-  navbar.previousElementSibling.classList.add('smooth-animation--1');
-
   if (menuToggle.checked) {
     backdrop.classList.add('backdrop-display');
-    mainBody.classList.add('scroll-view-shrink');
-    navbar.classList.add('navigation-move');
-    navbar.previousElementSibling.classList.add('navigation-move');
+    animate({
+      time: 0.5,
+      run: rate => {
+        factor = 1 - rate;
+        mainBody.style.width = rate * +sideDrawerWidth + +mainBodyShrinkSize + 'px';
+        navbar.style.transform = `translateX(${factor * +sideDrawerWidth}px)`;
+        navbar.previousElementSibling.style.transform = `translateX(${
+          factor * +sideDrawerWidth
+        }px)`;
+        backdrop.style.opacity = factor * 1;
+      },
+    });
   } else {
-    mainBody.classList.remove('scroll-view-shrink');
-    navbar.classList.remove('navigation-move');
-    navbar.previousElementSibling.classList.remove('navigation-move');
+    animate({
+      time: 0.5,
+      run: rate => {
+        factor = 1 - rate;
+        mainBody.style.width = rate * +sideDrawerWidth + +mainBodyShrinkSize + 'px';
+        navbar.style.transform = `translateX(${factor * +sideDrawerWidth}px)`;
+        navbar.previousElementSibling.style.transform = `translateX(${
+          factor * +sideDrawerWidth
+        }px)`;
+        backdrop.style.opacity = factor * 1;
+      },
+      backward: true,
+      onComplete: () => {
+        backdrop.classList.remove('backdrop-display');
+      },
+    });
   }
 };
 
@@ -108,74 +96,52 @@ const moveAndOtherTouchListenersRemoved = ev => {
   let dx = ev.changedTouches[0].clientX - x0;
   let s = Math.sign(dx);
 
-  menuToggle.classList.remove('checkbox-transition');
+  if ((s > 0 && factor.toFixed(2) > 0.25) || (s < 0 && factor.toFixed(2) >= 0.75)) {
+    menuToggle.checked = true;
 
-  if (s > 0) {
-    if (factor.toFixed(2) > 0.25) {
-      startfps();
-      menuToggle.classList.add('.button-icon-animation--2');
-      mainBody.classList.add('smooth-animation--2');
-      navbar.classList.add('smooth-animation--2');
-      navbar.previousElementSibling.classList.add('smooth-animation--2');
-
-      menuToggle.checked = true;
-
-      mainBody.classList.add('scroll-view-shrink');
-      navbar.classList.add('navigation-move');
-      navbar.previousElementSibling.classList.add('navigation-move');
-    } else {
-      startfps();
-      backdrop.classList.remove('backdrop-display');
-
-      menuToggle.classList.add('.button-icon-animation--3');
-      mainBody.classList.add('smooth-animation--3');
-      navbar.classList.add('smooth-animation--3');
-      navbar.previousElementSibling.classList.add('smooth-animation--3');
-
-      menuToggle.checked = false;
-    }
+    animate({
+      time: 0.5 * (1 - factor),
+      run: rate => {
+        factor = mainBody.offsetLeft / +sideDrawerWidth;
+        mainBody.style.width =
+          rate * (mainBody.clientWidth - +mainBodyShrinkSize) + +mainBodyShrinkSize + 'px';
+        navbar.style.transform = `translateX(${factor * +sideDrawerWidth}px)`;
+        navbar.previousElementSibling.style.transform = `translateX(${
+          factor * +sideDrawerWidth
+        }px)`;
+        backdrop.style.opacity = factor * 1;
+      },
+    });
   }
 
-  if (s < 0) {
-    if (factor.toFixed(2) < 0.75) {
-      startfps();
-      menuToggle.classList.add('.button-icon-animation--3');
-      mainBody.classList.add('smooth-animation--3');
-      navbar.classList.add('smooth-animation--3');
-      navbar.previousElementSibling.classList.add('smooth-animation--3');
+  if ((s < 0 && factor.toFixed(2) < 0.75) || (s > 0 && factor.toFixed(2) <= 0.25)) {
+    menuToggle.checked = false;
 
-      menuToggle.checked = false;
-
-      mainBody.classList.remove('scroll-view-shrink');
-    } else {
-      startfps();
-      menuToggle.classList.add('.button-icon-animation--2');
-      mainBody.classList.add('smooth-animation--2');
-      navbar.classList.add('smooth-animation--2');
-      navbar.previousElementSibling.classList.add('smooth-animation--2');
-
-      navbar.classList.add('navigation-move');
-      navbar.previousElementSibling.classList.add('navigation-move');
-
-      menuToggle.checked = true;
-    }
+    animate({
+      time: 0.5 * factor,
+      run: rate => {
+        factor = mainBody.offsetLeft / +sideDrawerWidth;
+        mainBody.style.width = bodySizes.width - rate * mainBody.offsetLeft + 'px';
+        navbar.style.transform = `translateX(${factor * +sideDrawerWidth}px)`;
+        navbar.previousElementSibling.style.transform = `translateX(${
+          factor * +sideDrawerWidth
+        }px)`;
+        backdrop.style.opacity = factor * 1;
+      },
+      onComplete: () => {
+        backdrop.classList.remove('backdrop-display');
+      },
+    });
   }
 
   x0 = null;
-  document.body.style.removeProperty('--main-body-change-size');
+  changedBodySize = null;
 
   window.removeEventListener('touchmove', touchDrag);
   window.removeEventListener('touchend', moveAndOtherTouchListenersRemoved);
 };
 
 const touchDrag = ev => {
-  clearInterval(fps);
-  clearInterval(interval1);
-  clearTimeout(timeout1);
-  interval1 = null;
-  timeout1 = null;
-  fps = null;
-
   let dx = ev.changedTouches[0].clientX - x0;
   let s = Math.sign(dx);
   let changedBodySize = null;
@@ -187,32 +153,30 @@ const touchDrag = ev => {
   }
 
   if (s > 0 && !menuToggle.checked) {
-    menuToggle.classList.add('checkbox-transition');
-    backdrop.classList.add('backdrop-display');
-    changedBodySize = (bodySizes.width - dxa).toFixed(2);
-    document.body.style.setProperty('--main-body-change-size', changedBodySize + 'px');
-    onChangeInMainBodyWidth();
+    if (![...backdrop.classList].includes('backdrop-display')) {
+      backdrop.classList.add('backdrop-display');
+    }
+    factor = dxa / +sideDrawerWidth;
+    changedBodySize = bodySizes.width - dxa;
+
+    navbar.style.transform = `translateX(${dxa}px)`;
+    navbar.previousElementSibling.style.transform = `translateX(${dxa}px)`;
   }
   if (s < 0 && menuToggle.checked) {
-    menuToggle.classList.add('checkbox-transition');
-    navbar.classList.remove('navigation-move');
-    navbar.previousElementSibling.classList.remove('navigation-move');
-    changedBodySize = (+mainBodyShrinkSize + dxa).toFixed(2);
-    document.body.style.setProperty('--main-body-change-size', changedBodySize + 'px');
-    onChangeInMainBodyWidth();
+    const diff = +sideDrawerWidth - dxa;
+    factor = diff / +sideDrawerWidth;
+    changedBodySize = +mainBodyShrinkSize + dxa;
+
+    navbar.style.transform = `translateX(${diff}px)`;
+    navbar.previousElementSibling.style.transform = `translateX(${diff}px)`;
+  }
+  if ((s > 0 && !menuToggle.checked) || (s < 0 && menuToggle.checked)) {
+    mainBody.style.width = changedBodySize + 'px';
+    backdrop.style.opacity = factor * 1;
   }
 };
 
 const lockAndOtherTouchListenersAdded = ev => {
-  clearInterval(fps);
-  clearInterval(interval1);
-  clearTimeout(timeout1);
-  interval1 = null;
-  timeout1 = null;
-  fps = null;
-  if (!ev.changedTouches) {
-    return;
-  }
   x0 = ev.changedTouches[0].clientX;
   window.addEventListener('touchmove', touchDrag);
   window.addEventListener('touchend', moveAndOtherTouchListenersRemoved);
@@ -221,7 +185,7 @@ const lockAndOtherTouchListenersAdded = ev => {
 
 // Positioning hero elements realative to the phone-bezel
 const posHandler = () => {
-  const psuedoElementHeight = +window
+  const bezelPsuedoElementHeight = +window
     .getComputedStyle(phoneBezelDiv, '::before')
     .getPropertyValue('height')
     .slice(0, -2);
@@ -230,12 +194,12 @@ const posHandler = () => {
 
   const scrolledPosition = mainBody.scrollTop;
   const editedPhoneBezelDivSizes = {
-    height: phoneBezelDivSizes.height - psuedoElementHeight * 2,
+    height: phoneBezelDivSizes.height - bezelPsuedoElementHeight * 2,
     width: phoneBezelDivSizes.width,
     x: phoneBezelDivSizes.x - bodySizes.x,
-    y: phoneBezelDivSizes.y + psuedoElementHeight + scrolledPosition,
-    top: phoneBezelDivSizes.top + psuedoElementHeight + scrolledPosition,
-    bottom: phoneBezelDivSizes.bottom - psuedoElementHeight + scrolledPosition,
+    y: phoneBezelDivSizes.y + bezelPsuedoElementHeight + scrolledPosition,
+    top: phoneBezelDivSizes.top + bezelPsuedoElementHeight + scrolledPosition,
+    bottom: phoneBezelDivSizes.bottom - bezelPsuedoElementHeight + scrolledPosition,
     left: phoneBezelDivSizes.left - bodySizes.x,
     right: phoneBezelDivSizes.right - bodySizes.x,
   };
@@ -266,22 +230,10 @@ const posHandler = () => {
   );
 };
 
-// Adding a listener to changeInMainBodyWidth
-mainBody.addEventListener('changeInMainBodyWidth', onChangeInMainBodyWidth);
 // Adding an onscroll listener
 mainBody.addEventListener('scroll', onScroll);
 // Adding an onchange listener to menu button
-menuToggle.addEventListener('change', () => {
-  clearInterval(fps);
-  clearInterval(interval1);
-  clearTimeout(timeout1);
-  interval1 = null;
-  timeout1 = null;
-  fps = null;
-
-  menuToggleClickHandler();
-  startfps();
-});
+menuToggle.addEventListener('change', menuToggleClickHandler);
 // Adding listener for touch-screen users
 window.addEventListener('touchstart', lockAndOtherTouchListenersAdded, false);
 
@@ -294,22 +246,26 @@ const ready = () => {
 
   // Giving vw unit to the body and setting the size of the side drawer
   sideDrawer.style.width = sideDrawerWidth + 'px';
+  navbar.style.transform = `translateX(${mainBody.offsetLeft}px)`;
+  navbar.previousElementSibling.style.transform = `translateX(${mainBody.offsetLeft}px)`;
   document.body.style.setProperty('--vw', bodySizes.width / 100 + 'px');
-  document.body.style.setProperty('--main-body-shrink-size', mainBodyShrinkSize + 'px');
+  // document.body.style.setProperty('--main-body-shrink-size', mainBodyShrinkSize + 'px');
   document.body.style.setProperty('--side-drawer-width', sideDrawerWidth + 'px');
 
-  clearInterval(interval1);
-  clearTimeout(timeout1);
-  interval1 = null;
-  timeout1 = null;
-
-  interval1 = setInterval(posHandler, 16);
-  timeout1 = setTimeout(() => {
-    clearInterval(interval1);
-    clearTimeout(timeout1);
-    interval1 = null;
-    timeout1 = null;
-  }, 50);
+  fps = null;
+  timeoutCounter = 0;
+  let startfps = () => {
+    posHandler();
+    fps = requestAnimationFrame(startfps);
+    if (timeoutCounter >= 3) {
+      cancelAnimationFrame(fps);
+      fps = null;
+      timeoutCounter = null;
+      startfps = null;
+    }
+    timeoutCounter++;
+  };
+  fps = requestAnimationFrame(startfps);
 };
 
 window.onload = ready;
